3bfc0a3b80ca55e750ba271178bc347230b7307e
navcsdev <nguyenanhvu.cs@gmail.com>
2021-09-27 00:37:00 +0700

    Fix cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::API
   include Pagy::Backend
-
-  config.middleware.insert_before 0, "Rack::Cors" do
-    allow do
-      origins '*'
-      resource '*', :headers => :any, :methods => [:get, :post, :options]
-    end
-  end
 end
